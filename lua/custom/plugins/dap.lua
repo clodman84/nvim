80f8b1e4c248de884f0e4749c9ac98d940982677
clodman84 <cshourin@gmail.com>
2025-05-04 20:28:34 +0530

    fresh start

--- lua/custom/plugins/dap.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'mfussenegger/nvim-dap',
-  'jay-babu/mason-nvim-dap.nvim',
-  config = function()
-    require('mason').setup()
-    require('mason-nvim-dap').setup()
-  end,
-}
---
+++ todo.md
@@ -0,0 +1,4 @@
+# install luarocks
+# install ripgrep
+# install and setup telescope
+# remove the base plugin setup and make it from scratch
