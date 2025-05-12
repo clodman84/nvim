0b9e2b8d2ea664119161cc2e25f513e422bc2214
clodman84 <cshourin@gmail.com>
2025-05-13 00:26:23 +0530

    added lazygit for some reason
    lmao this shit is insane

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,3 +1,9 @@
 return {
-  {'akinsho/toggleterm.nvim', version = "*", opts = {--[[ things you want to change go here]]}}
+	{
+		'akinsho/toggleterm.nvim',
+		version = "*",
+		opts = {
+			open_mapping = "<leader>t"
+		}
+	}
 }
